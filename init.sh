0e9a350a9ade83594ef54c4af3182067b6ca290b
Prasanna Kelkar <prankkelkar@gmail.com>
2019-06-15 10:17:50 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 cd /uber
+cp config.json /root/Python-Sample-Application/
+cd /root/Python-Sample-Application
 export UBER_CLIENT_ID=`jq '.UBER_CLIENT_ID' config.json`
 export UBER_CLIENT_SECRET=`jq '.UBER_CLIENT_SECRET' config.json`
-cd /root/Python-Sample-Application
 python app.py
+
